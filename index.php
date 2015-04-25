882bdc21f8174779e19fc2d2db45d6ac5eac045a
trineon89 <trineon89@gmail.com>
2015-04-26 00:43:24 +0200

    updated frontpage + few fixes

--- index.php
+++ index.php
@@ -29,15 +29,19 @@
 	<section id="mainself">
 		<article id="featuredimage">	<?php //require_once('iframe_cssslider.html');	?><iframe src="cssslider.html" style="width:597px;height:280px;max-width:100%;overflow:hidden;border:none;padding:0;margin:0 auto;display:block;" scrolling="no" marginheight="0" marginwidth="0"></iframe>
 		</article>
-		<a href="page.php?page=benevol"><article id="benevol">&nbsp;</article></a>
-		<a href="page.php?page=don"><article id="don"><div id="don_background">
-			<header>Faites un don pour assister AutismeLuxembourg!</header>
-			<p id="virement">Virement bancaire</p>
-			<p id="virementcode">BCEE LU99 0000 1111 2222 3333<br/>BGLLUL LU12 1234 5678 9101 1013</p>
-		</div> <!-- background Donenconfiance -->
-		</article></a>
 		<a href="page.php?page=slogan" class="slogan"><article id="ecoute"><p id="ecoute_p">Ecoute</p><p id="competence">Compétence</p><p id="dynamisme">Dynamisme</p></article></a>
 	</section>
+	<div class="clearfloat">
+	</div>
+	
+	<div class="secondmain">
+		<a href="page.php?page=benevol"><article id="benevol">&nbsp;</article></a>
+			<a href="page.php?page=don"><article id="don">
+				<p id="virement">Virement bancaire</p>
+				<p id="virementcode">BCEE LU99 0000 1111 2222 3333<br/>BGLLUL LU12 1234 5678 9101 1013</p>
+			</article></a>
+	</div>
+		
 	<div class="clearfloat">
 	</div>
 	<? require_once('incl/footer.inc.php'); ?>
